91a1608437618140b309fb9d8338195f14e8255b
CEOS4C <68231770+CEOS4C@users.noreply.github.com>
2024-12-11 12:21:25 +0100

    Update Problemes.java

--- src/Problemes.java
+++ src/Problemes.java
@@ -70,68 +70,90 @@ public class Problemes {
         }
     }
 
-    public void interroger(){
-
-        if (eleve.getCurrentUV().getName().equals("PC20")){
-            System.out.println("Veuillez rentrer une hypothese:");
-            Scanner sc = new Scanner(System.in);
-            String hypothese = sc.nextLine().trim();
-
-            while (!isValidGuess(hypothese)){
-                System.out.println("Entrée non valide, veuillez rentrer une chaine de 4 chiffres:");
-                hypothese = sc.nextLine();
-            }
-
-            giveFeedbackMasterMind(hypothese);
+    public void interroger() {
+        if (eleve.getCurrentUV().getName().equals("PC20")) {
+            giveFeedbackMasterMind();
         }
 
-        if (eleve.getCurrentUV().getName().equals("MT3F")){
-            UV currentUV = eleve.getCurrentUV();
-            String solution = currentUV.getListeProblemes().get(enonce);
-            if (solution.equals("-143")){
-                System.out.println("\nLe déterminant d'une matrice 3×3 se calcule en multipliant les éléments de la première ligne \n" +
-                        "par les déterminants des matrices 2×2 restantes, en alternant les signes. \n" +
-                        "Il s'agit de prendre chaque élément de la première ligne, \n" +
-                        "multiplier par le déterminant de la sous-matrice 2×2 correspondante, \n" +
-                        "et additionner ou soustraire ces produits en fonction de leur position (alternance des signes).\n");
+        if (eleve.getCurrentUV().getName().equals("MT3F")) {
+            giveFeedbackMT3F();
+        }
+    }
 
-                System.out.print("Veuillez rentrer une hypothese :");
-                Scanner sc = new Scanner(System.in);
-                String hypothese = sc.nextLine();
+    public void giveFeedbackMT3F() {
+        UV currentUV = eleve.getCurrentUV();
+        String solution = currentUV.getListeProblemes().get(enonce);
 
-                if (hypothese.matches("\\d+")){
-                    System.out.println("Le nombre à trouver est négatif.\n");
-                }
-                else if (!hypothese.matches("\\d{3}")){
-                    System.out.println("Le nombre à trouver est composé de 3 chiffres.\n");
-                }
+        if (solution.equals("-143")) {
+            System.out.println("\nLe déterminant d'une matrice 3×3 se calcule en multipliant les éléments de la première ligne \n" +
+                    "par les déterminants des matrices 2×2 restantes, en alternant les signes. \n" +
+                    "Il s'agit de prendre chaque élément de la première ligne, \n" +
+                    "multiplier par le déterminant de la sous-matrice 2×2 correspondante, \n" +
+                    "et additionner ou soustraire ces produits en fonction de leur position (alternance des signes).\n");
+
+            System.out.print("Veuillez rentrer une hypothese :");
+            Scanner sc = new Scanner(System.in);
+            String hypothese = sc.nextLine();
+
+            if (hypothese.matches("\\d+")) {
+                System.out.println("Le nombre à trouver est négatif.\n");
+            } else if (!hypothese.matches("\\d{3}")) {
+                System.out.println("Le nombre à trouver est composé de 3 chiffres.\n");
+            } else {
+                System.out.println("Pense à bien relire les aides données.\n");
             }
-            else if (solution.equals("1/2*ln(x^2+1)+c")){
-                System.out.println("\nPour résoudre cette intégrale, il faut effectuer un changement de variable.\n" +
-                        "On pose u = x^2 + 1, donc du = 2x dx.\n");
+        } else if (solution.equals("1/2*ln(x^2+1)+c")) {
+            System.out.println("\nPour résoudre cette intégrale, il faut effectuer un changement de variable.\n" +
+                    "On pose u = x^2 + 1, donc du = 2x dx.\n");
 
-                System.out.print("Veuillez rentrer une hypothèse : ");
-                Scanner sc = new Scanner(System.in);
-                String hypothese = sc.nextLine();
+            System.out.print("Veuillez rentrer une hypothèse : ");
+            Scanner sc = new Scanner(System.in);
+            String hypothese = sc.nextLine();
 
-                if (hypothese.matches("1/2*ln(x^2+1)")){
-                    System.out.println("N'oublie pas la constante.\n");
+            if (hypothese.matches("1/2*ln(x^2+1)")) {
+                System.out.println("N'oublie pas la constante.\n");
+            } else if (hypothese.matches("ln(x^2+1)+c")) {
+                System.out.println("N'oubliez pas le facteur devant le ln.\n");
+            } else {
+                System.out.println("Pense à bien relire les aides données.\n");
+            }
+        } else if (solution.equals("9,75")) {
+            System.out.println("\nPour résoudre ce produit infini, il faut le développer.\n" +
+                    "On peut remarquer que le terme général est de la forme 1 + 1/(2n-1).\n" +
+                    "Il faut donc développer ce produit jusqu'à n=30 et arrondir à 2 décimales supérieures.\n");
+
+            System.out.print("Veuillez rentrer une hypothèse : ");
+            Scanner sc = new Scanner(System.in);
+            String hypothese = sc.nextLine();
+
+            if (!hypothese.matches("^\\d+(\\.\\d{2})?$")) {
+                System.out.println("L'hypothèse doit être un nombre à deux décimales.\n");
+            } else if (hypothese.matches("^9\\,7[0-9]$")) {
+                char secondDecimal = hypothese.charAt(3);
+                if (secondDecimal != '5') {
+                    System.out.println("L'arrondi n'est pas correct.\n");
                 }
-                else if (hypothese.matches("ln(x^2+1)+c")){
-                    System.out.println("N'oubliez pas le facteur devant le ln.\n");
-                }
-                else {
-                    System.out.println("Pense a bien relire les aides données.\n");
-                }
+            } else if (hypothese.matches("^\\d\\,[0-9]+$")) {
+                System.out.println("L'unité doit être un seul chiffre avant la virgule.");
+            } else {
+                System.out.println("Pensez à bien relire les aides données.\n");
             }
         }
     }
 
-    private static boolean isValidGuess(String guess) {
+    public static boolean isValidGuess(String guess) {
         return guess.matches("\\d{4}"); // Vérifie que la chaîne est composée de 4 chiffres
     }
 
-    private void giveFeedbackMasterMind(String hypothese) {
+    public void giveFeedbackMasterMind() {
+        System.out.println("Veuillez rentrer une hypothese:");
+        Scanner sc = new Scanner(System.in);
+        String hypothese = sc.nextLine().trim();
+        while (!isValidGuess(hypothese)) {
+            System.out.println("Entrée non valide, veuillez rentrer une chaine de 4 chiffres:");
+            hypothese = sc.nextLine();
+        }
+
         UV currentUV = eleve.getCurrentUV();
         String solution = currentUV.getListeProblemes().get(enonce);
         int bulls = 0; // Bons chiffres à la bonne position
